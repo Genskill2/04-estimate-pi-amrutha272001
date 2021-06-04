8a67e9bbefc9aa04170bc5830ce006c218e9cf1b
amrutha272001 <84664990+amrutha272001@users.noreply.github.com>
2021-06-04 13:33:00 +0530

    monte

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,21 +3,18 @@
 #include <stdio.h>
 #include <math.h>
 float mc_pi(int n){	
-	int pc=0,ps=0;
+	int pc=0;
 	float x,y,len,pi;
-	for(int i=0; i<n; i++)
-	{
+	for(int i=0; i<n; i++){
 	  x=frandom();
 	  y=frandom();
 	  len =sqrt(x*x+y*y);
 	 if(len<1){
 	  pc++;
+	  }
+
 	}
-         else{
-          ps++;
-	}
-	}
-	pi = 4* ((float)pc/(ps+pc));
+	pi = 4 * ((float)pc/n);
 	return pi;
     }
 float frandom()
