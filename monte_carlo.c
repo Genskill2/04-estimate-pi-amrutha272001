a0a9febd0097029dd09e954bb0686b237c674e61
amrutha272001 <84664990+amrutha272001@users.noreply.github.com>
2021-06-04 11:45:24 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -5,6 +5,22 @@
 
 float mc_pi(int);
 
+float mc_pi(int n)
+{	
+	int pc=0,ps=n;
+	
+	for(int i=0; i<n; i++){
+	float x,y,len;
+	x=frandom();
+	y=frandom();
+	len =x*x+y*y;
+	if(len<=1){
+	pc++;
+	}
+	}
+	float pi = 4.0*pc/ps;
+	return pi;
+}
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
