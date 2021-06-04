262b8d2b6410f4e1300c1e7331814004ca2fdbcd
amrutha272001 <84664990+amrutha272001@users.noreply.github.com>
2021-06-04 12:02:37 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 #include <math.h>
 float wallis_pi(int);
-float wallis_pi(int n)
-{
-  float k=1.0;
-  int i;
-  for (i=1;i<n;i++)
-    {
-    k*=(float)(4.*i*i)/(4.*i*i-1);
-    }
-  return (k*2);
-  }
+ float wallis_pi(int n);
+ {	float pi;
+ 	float prod=1;
+ 	for(int i=1;i<=n;i++){
+ 	float m = (4.0*i*i)/((4.0*i*i)-1);
+ 	prod=prod*m;
+ 	}
+ 	pi=2*prod;
+ 	return pi;
+}
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
