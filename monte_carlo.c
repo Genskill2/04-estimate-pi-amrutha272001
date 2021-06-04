c9536ddf37a73eb042b8ff10354a57e42ffe98c2
amrutha272001 <84664990+amrutha272001@users.noreply.github.com>
2021-06-04 13:54:20 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -1,60 +1,38 @@
-#include <assert.h>
+ #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
-  
-    
-float frandom()
-{
-  long int q = random();
-  float ret = (float)q/(float)RAND_MAX;
-  return ret;
-}
-int main(void)
-{
-  float pi0;
-  float pi1;
-  pi0 = mc_pi(25000);
-  pi1 = mc_pi(25000);
-  printf("%f %f\n", pi0, pi1);
-  if (pi0 == pi1) 
-  {
-      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
+float wallis_pi(int);
+
+int main(void) {
+  float pi;
+  for (int i=0; i<5; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
+    }
   }
-  if (fabs(pi0 - pi1) > 0.05) 
-  {
-      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
-      abort();
-  }
-  for (int i=2000; i<5000; i++)
-  {
-    pi0 = mc_pi(i);
-    if (!(fabs(pi0 - M_PI) < 0.4))
-    {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
+
+  for (int i=500; i<3000; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
       abort();
     }
   }
 }
 
-float mc_pi(int n){
-        int pc=0,ps=0;
-        float x,y,len;
-        for(int i=0;i<n;i++){
-               x=frandom();
-               y=frandom();
-               len=sqrt((x*x) + ( y*y));
-             if(len < 1){
-                    pc++;
-                }
-          else{
-            ps++;
-          }
-             float pi= 4* ((float)pc/(pc+ps));
-              return pi;
-        }
+float  wallis_pi(int n){
+  float y,x=1.00; 
+  for(int a=1 ; a<=n ; a++){
+    y =(float) (4*a*a) / (4*a*a -1);
+    x=x*y;
+  }
+  x=2*x;
+  return x;
+}
+            
 
 
